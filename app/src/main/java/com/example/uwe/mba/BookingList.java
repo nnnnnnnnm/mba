ec14853c21883e0565ee33acfe7b7b3f8830c823
nnnnnnnnm <dickychchan@gmail.com>
2017-10-16 17:57:36 +0800

    v5
    gg

--- app/src/main/java/com/example/uwe/mba/BookingList.java
+++
@@ -1,8 +0,0 @@
-package com.example.uwe.mba;
-
-/**
- * Created by Hin on 16/10/2017.
- */
-
-public class BookingList {
-}
--- app/src/main/java/com/example/uwe/mba/UserList.java
+++
@@ -1,8 +0,0 @@
-package com.example.uwe.mba;
-
-/**
- * Created by Dicky on 16/10/2017.
- */
-
-public class UserList {
-}
